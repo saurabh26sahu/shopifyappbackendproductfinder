0c07a7b609966cd2ae68a04a1b0456e83c21d155
saurabh <saurabh@coderapper.com>
2018-04-24 15:56:22 +0530

    Titles insertion done
    products title insertion done

--- get_products.php
+++ get_products.php
@@ -20,62 +20,40 @@
 		$title_tags = implode(',', $product_tags);
 		$product_type = array_column($products, 'product_type');
 		$title_type = implode(',', $product_type);
-	
+		//Mysql Connection Starts
 		$servername = "localhost";
 		$username = "root";
 		$password = "";
 		$dbname = "shopifyapp";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+     	// Create connection
+		$conn = mysqli_connect($servername, $username, $password, $dbname);
+		// Check connection
+		if (!$conn) {
+    	die("Connection failed: " . mysqli_connect_error());
+		}
 
-if(is_array($product_title)){
-	    $product_title = array_column($products, 'title');
-		$title_title = implode(',', $product_title);
-		$product_handle = array_column($products, 'handle');
-		//$title_handle = implode(',', $product_handle);
-		$product_tags = array_column($products, 'tags');
-		//$title_tags = implode(',', $product_tags);
-		$product_type = array_column($products, 'product_type');
-		//$title_type = implode(',', $product_type);
+		//Data Insertion Starts
+		if(is_array($product_title)){
+    	foreach ($product_title as $row) {      
+        $query ="INSERT INTO products_data (title) VALUES ( '". $row."')";
+        mysqli_query($conn, $query);
+    	}
+	}
 
-    $DataArr = array($product_title);
-   // print_r($DataArr);
+  	if (mysqli_query($conn, $query)) 
+	{
+    echo "App Data Inserted Successfully";
+    } 
 
-    foreach($product_title as $row){
-    	echo "<pre>";
-    	//print_r($row);
-       // echo $product_title = mysql_real_escape_string($products[$row]['title']);
-       // $product_handle= mysql_real_escape_string($products[$row]['product_type']);
-       //  $product_tags= mysql_real_escape_string($products[$row]['handle']);
-    //$DataArr[] = "('$row')";
-    
-//$data = implode(',', $DataArr);
-    	$title_title = implode(',', $product_title);
-   //echo $data;
-        
+	else {
+    echo "Error: " . $query . "<br>" . mysqli_error($conn);
     }
-    $sql = "INSERT INTO products_data (title) values ($title_title)";
-    mysqli_query($conn, $sql); 
-}
 
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
-
-		// echo "<pre>";
-		// print_r($products);
+	mysqli_close($conn);
 
 	}
+
 	catch (shopify\ApiException $e)
 	{
 		# HTTP status code was >= 400 or response contained the key 'errors'
@@ -90,3 +68,4 @@ $conn->close();
 		print_r($e->getRequest());
 		print_r($e->getResponse());
 	}
+
